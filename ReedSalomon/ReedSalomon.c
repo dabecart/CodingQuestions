d41fd4f91179086fe63d5cba8247f3ea549bc005
dabecart <dbejarcaballero@gmail.com>
2024-07-07 14:41:53 +0200

    Done everything in modular mode, so it's very memory efficient. Success rate of  99.83% at avg 3.58ms/correction.

--- ReedSalomon/ReedSalomon.c
+++ ReedSalomon/ReedSalomon.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-#define RS_MAX_POLY_DEGREE 10
+#define RS_MAX_POLY_DEGREE 13
 #define MODULUS 257
 #define EXTRA_POINTS 3
+#define BAR_WIDTH 50
+
+// If we take that the extra points are not corrupted, the computations are easier and faster to do.
+int EEPROM_NOT_CORRUPTED = 1;
 
 /***************************************************************************************************
  * BASIC MATH
@@ -28,6 +33,23 @@ int mod(int x){
     return x % MODULUS;
 }
 
+int parity(int x){
+    int y = x ^ (x >> 1);
+    y     = y ^ (y >> 2);
+    y     = y ^ (y >> 4);
+    y     = y ^ (y >> 8);
+    y     = y ^ (y >> 16);
+    return y & 1;
+}
+
+int arrayParity(int* x, int len){
+    int par = 0;
+    for(int i = 0; i < len; i++){
+        par ^= parity(x[i]);
+    }
+    return par;
+}
+
 /***************************************************************************************************
  * FRACTION
  **************************************************************************************************/
@@ -37,32 +59,6 @@ typedef struct{
 
 const Fraction ZERO = {0,1};
 const Fraction ONE  = {1,1};
-
-Fraction reduceFrac(Fraction x){
-    if(x.b == 0){
-        printf("Dividing by zero!");
-        exit(-1);
-    }
-    int commonDivisor = gcd(x.a, x.b);
-    x = (Fraction) {x.a/commonDivisor, x.b/commonDivisor};
-    if(x.b < 0){
-        x.b = -x.b;
-        x.a = -x.a;
-    }
-    return x;
-}
-
-Fraction sumFrac(Fraction x, Fraction y){
-    return reduceFrac((Fraction){x.a*y.b + y.a*x.b, x.b*y.b});
-}
-
-Fraction subsFrac(Fraction x, Fraction y){
-    return reduceFrac((Fraction){x.a*y.b - y.a*x.b, x.b*y.b});
-}
-
-Fraction multFrac(Fraction x, Fraction y){
-    return reduceFrac((Fraction){x.a*y.a, x.b*y.b});
-}
 
 // Instead of doing it by brute force, use the Extended Euclidean algorithm?
 Fraction modFrac(Fraction x){
@@ -95,6 +91,35 @@ Fraction modFrac(Fraction x){
     x.a = mod(x.a*n);
     x.b = 1;
     return x;
+}
+
+Fraction reduceFrac(Fraction x){
+    if(x.b == 0){
+        printf("Dividing by zero!");
+        exit(-1);
+    }
+    int commonDivisor = gcd(x.a, x.b);
+    x = (Fraction) {x.a/commonDivisor, x.b/commonDivisor};
+    if(x.b < 0){
+        x.b = -x.b;
+        x.a = -x.a;
+    }
+
+    x = modFrac(x);
+
+    return x;
+}
+
+Fraction sumFrac(Fraction x, Fraction y){
+    return reduceFrac((Fraction){x.a*y.b + y.a*x.b, x.b*y.b});
+}
+
+Fraction subsFrac(Fraction x, Fraction y){
+    return reduceFrac((Fraction){x.a*y.b - y.a*x.b, x.b*y.b});
+}
+
+Fraction multFrac(Fraction x, Fraction y){
+    return reduceFrac((Fraction){x.a*y.a, x.b*y.b});
 }
 
 int equalFractions(Fraction x, Fraction y){
@@ -143,7 +168,7 @@ Polynomial createEmptyPoly(int degree){
 }
 
 Polynomial reducePoly(Polynomial p){
-    while(p.coeffs[p.degree].a == 0){
+    while(p.coeffs[p.degree].a == 0 && p.degree > 0){
         p.degree--;
     }
     return p;
@@ -250,6 +275,19 @@ Polynomial createLagrangeInterp(int* x, int* y, int count){
 }
 
 /***************************************************************************************************
+ * HAMMING CODE (with whole numbers)
+ **************************************************************************************************/
+int calculateHamming(int* x, int* y, int len){
+    int hamming = 0;
+    for(int i = 0; i < len; i++){
+        if(parity(y[i])){
+            hamming ^= x[i];
+        }        
+    } 
+    return hamming;
+}
+
+/***************************************************************************************************
  * ERROR CORRECTION ALGORITHM
  **************************************************************************************************/
 
@@ -261,10 +299,10 @@ int checkPoints(int* rx, int* ry, int len, int pointsPerLagrange, int* indices){
     int y[pointsPerLagrange];
     for(int i = 0; i < pointsPerLagrange; i++){
         x[i] = rx[indices[i]];
-        printf("%d", x[i]);
+        // printf("%d,", x[i]);
         y[i] = ry[indices[i]];
     }
-    printf("\n");
+    // printf("\n");
 
     Polynomial p = createLagrangeInterp(x, y, pointsPerLagrange);
 
@@ -281,7 +319,14 @@ int checkPoints(int* rx, int* ry, int len, int pointsPerLagrange, int* indices){
                 // not valid.
                 return -1;
             }else{
-                pointsNotOk += eval.a != ry[i];
+                int pointNotOK = eval.a != ry[i];
+                
+                // If the EEPROM is OK and this comparator is saying that a point in EEPROM is wrong
+                // skip it, as this function isn't correct.
+                if(EEPROM_NOT_CORRUPTED && pointNotOK && i >= (len - EXTRA_POINTS)){
+                    return -1;
+                }
+                pointsNotOk += pointNotOK; 
             }
         }else{
             if(j < (pointsPerLagrange-1)) j++;
@@ -295,12 +340,33 @@ int checkPoints(int* rx, int* ry, int len, int pointsPerLagrange, int* indices){
     // No errors found!
     if(pointsNotOk == 0)               return 0;
 
+    int tempSave[len];
+    memcpy(tempSave, ry, len*sizeof(int));
+
     // Errors were found, but can be fixed by evaluating the polynomial.
     for(i = 0; i < len; i++){
         Fraction eval = evaluatePoly(p, (Fraction){rx[i], 1});
         eval = modFrac(eval);
         ry[i] = eval.a;
     }
+
+    // If the EEPROM isn't corrupted, use the Hamming.
+    if(EEPROM_NOT_CORRUPTED){
+        // Check the Hamming. The Hamming is sent after the EXTRA_POINTS in the array.
+        // If the message is the same, when XORing the Hamming, it should return 0.
+        int newHamming = calculateHamming(rx,ry, len) ^ (ry[len] & 0x7F);
+        // Calculate the parity of the whole message. Compare it with the parity of the MSB of the 
+        // Hamming.
+        int newParity = arrayParity(ry, len) << 7; 
+        if(newHamming == 0 && newParity == (ry[len] & 0x80)){
+            return 1;
+        }
+
+        // Restore the points, Hamming wasn't correct.
+        memcpy(ry, tempSave, len*sizeof(int));
+        return -1;
+    }
+
     return 1;
 }
 
@@ -323,7 +389,20 @@ int doCombinations(int* rx, int* ry, int len, int pointsPerLagrange, int* indice
 
     for(int i = indexValue; i < len; i++){
         indices[indexPosition] = rx[i];
-        int ret = doCombinations(rx, ry, len, pointsPerLagrange, indices, i + 1, indexPosition + 1);
+
+        int nextIndex = i + 1;
+        // If we know the EEPROM is working, then we should always try to use the extra points 
+        // stored in it. So, when we have pointsPerLagrange-EXTRA_POINTS points from the actual 
+        // data, use the EEPROM points.
+        if(EEPROM_NOT_CORRUPTED && (indexPosition+1) == (pointsPerLagrange-EXTRA_POINTS)){
+            nextIndex = len - EXTRA_POINTS;
+            // If the next index is the current one, no more checks to do.
+            if(i >= nextIndex){
+                break;
+            }
+        }
+
+        int ret = doCombinations(rx, ry, len, pointsPerLagrange, indices, nextIndex, indexPosition + 1);
         // If the combination cannot be checked, continue searching for a new combination.
         // If no error was found or the error was fixed, no need to continue searching.
         if(ret != -1)   return ret;
@@ -333,7 +412,13 @@ int doCombinations(int* rx, int* ry, int len, int pointsPerLagrange, int* indice
 }
 
 int verifyMessage(int* rx, int* ry, int len, int pointsPerLagrange){
-    // Number of combinations is Bi<numberOfPoints, pointsPerCheck>, where Bi<a,b>=a!/b!/(a-b)!
+    // Using Bi<a,b>=a!/b!/(a-b)! ...
+    // Number of combinations when EEPROM is faulty: 
+    //    > Bi<len, pointsPerLagrange> 
+    // Number of combinations when EEPROM is OK: 
+    //    > Bi<len - EXTRA_POINTS, pointsPerLagrange - EXTRA_POINTS>
+    // Example:
+    //    > Using 10 points, with 3 EXTRA_POINTS: Faulty: 286 checks, Non faulty: 120 checks (~42%).
     int indices[pointsPerLagrange];
     return doCombinations(rx, ry, len, pointsPerLagrange, indices, 0, 0);
 }
@@ -343,6 +428,8 @@ int verifyMessage(int* rx, int* ry, int len, int pointsPerLagrange){
  **************************************************************************************************/
 
 int generateRandom(int lower, int upper) {
+    if(lower == upper) return lower;
+
     if (lower > upper) {
         int temp = lower;
         lower = upper;
@@ -354,22 +441,11 @@ int generateRandom(int lower, int upper) {
     return num;
 }
 
-int createSimulation(int minimumPoints, int maxPoints, int maxErrors){
-    int numPoints = generateRandom(minimumPoints, maxPoints);
-    int numErrors = generateRandom(0, maxErrors);
-
-    int x[numPoints];
-    int y[numPoints];
-
-    for(int i = 0; i < numPoints; i++){
-        x[i] = i;
-        y[i] = generateRandom(0,255);
-    }
-
+int runSimulation(int* x, int* y, int numPoints, int* errX, int* errY, int numErrors){
     Polynomial p = createLagrangeInterp(x, y, numPoints);
 
     int xx[numPoints + EXTRA_POINTS];
-    int yy[numPoints + EXTRA_POINTS];
+    int yy[numPoints + EXTRA_POINTS + 1];
 
     for(int i = 0; i < numPoints+EXTRA_POINTS; i++){
         xx[i] = i;
@@ -388,16 +464,21 @@ int createSimulation(int minimumPoints, int maxPoints, int maxErrors){
         yy[i] = result.a;
     }
 
+    // Add Hamming code.
+    yy[numPoints + EXTRA_POINTS] = calculateHamming(xx, yy, numPoints + EXTRA_POINTS);
+    // Add parity of the whole message at the MSB of the Hamming.
+    yy[numPoints + EXTRA_POINTS] |= arrayParity(yy, numPoints + EXTRA_POINTS) << 7;
+ 
     // Up to this point, the points are generated and "sent".
-    int errory[numPoints + EXTRA_POINTS];
+    int errory[numPoints + EXTRA_POINTS + 1];
     memcpy(errory, yy, sizeof(yy));
     
-    // New error introduced!
+    // New errors are introduced!
     for(int i = 0; i < numErrors; i++){
-        errory[generateRandom(0, numPoints + EXTRA_POINTS - 1)] = generateRandom(0,255);
+        errory[errX[i]] = errY[i];
     }
 
-    int ry[numPoints + EXTRA_POINTS];
+    int ry[numPoints + EXTRA_POINTS + 1];
     memcpy(ry, errory, sizeof(errory));
 
     // Find the error.
@@ -407,44 +488,133 @@ int createSimulation(int minimumPoints, int maxPoints, int maxErrors){
         for(int i = 0; i < numPoints+EXTRA_POINTS; i++){
             sameAsSent &= yy[i] == ry[i];
         }
-        if(!sameAsSent){
-            printf("points: %d\n", numPoints);
-            printPoly(p);
-            printf("NOT FIXED!\nX : ");
-            for(int i = 0; i < numPoints+EXTRA_POINTS; i++){
-                printf("%d, ", xx[i]);
-            }
-            printf("\nY : ");
-            for(int i = 0; i < numPoints+EXTRA_POINTS; i++){
-                printf("%d, ", yy[i]);
-            }
-            printf("\nEY: ");
-            for(int i = 0; i < numPoints+EXTRA_POINTS; i++){
-                printf("%d, ", errory[i]);
-            }
-            printf("\nRY: ");
-            for(int i = 0; i < numPoints+EXTRA_POINTS; i++){
-                printf("%d, ", ry[i]);
-            }
-            printf("\n\n");
-        }
-        return sameAsSent;
+        success = sameAsSent;
+        if(!success) printf("NOT FIXED!\n");
+    }else{
+        success = 0;
     }
-    return 0;
+
+    if(!success){
+        printf("Points: %d. Errors: %d\n", numPoints, numErrors);
+        printPoly(p);
+        printf("\nX :\t");
+        for(int i = 0; i < numPoints+EXTRA_POINTS; i++){
+            printf("%d,\t", xx[i]);
+        }
+        printf("\nY :\t");
+        for(int i = 0; i < numPoints+EXTRA_POINTS; i++){
+            printf("%d,\t", yy[i]);
+        }
+        printf("\nEY:\t");
+        for(int i = 0; i < numPoints+EXTRA_POINTS; i++){
+            if(yy[i] == errory[i]){
+                printf("\x1B[32m%d\x1B[0m,\t", errory[i]);
+            }else{
+                printf("\x1B[31m%d\x1B[0m,\t", errory[i]);
+            }
+        }
+        printf("\nRY:\t");
+        for(int i = 0; i < numPoints+EXTRA_POINTS; i++){
+            if(yy[i] == ry[i]){
+                printf("\x1B[32m%d\x1B[0m,\t", ry[i]);
+            }else{
+                printf("\x1B[31m%d\x1B[0m,\t", ry[i]);
+            }
+        }
+        printf("\n\n");
+    }
+
+    return success;
 }
 
-void testBench(){
+int createSimulation(int numPoints, int minErrors, int maxErrors){
+    int numErrors = generateRandom(minErrors, maxErrors);
+
+    int x[numPoints];
+    int y[numPoints];
+
+    for(int i = 0; i < numPoints; i++){
+        x[i] = i;
+        y[i] = generateRandom(0,255);
+    }
+
+    int errX[numErrors];
+    int errY[numErrors];
+
+    // New error introduced!
+    // TODO: Fix the points so that the X don't repeat!
+    for(int i = 0; i < numErrors; i++){
+        if(EEPROM_NOT_CORRUPTED){
+            errX[i] = generateRandom(0, numPoints - 1);
+        }else{
+            errX[i] = generateRandom(0, numPoints + EXTRA_POINTS - 1);
+        }
+        errY[i] = generateRandom(0,255);
+    }
+
+    return runSimulation(x, y, numPoints, errX, errY, numErrors);
+}
+
+void printLoadingBar(int progress, int total) {
+    int barLen = (progress * BAR_WIDTH) / total;
+    printf("Progress: [");
+    for (int i = 0; i < BAR_WIDTH; ++i) {
+        if (i < barLen) {
+            printf("#");
+        } else {
+            printf(" ");
+        }
+    }
+    printf("] %d%%\r", (progress * 100) / total);
+    fflush(stdout);
+}
+
+void testBench(int totalTests){
+    printf("Points per sample: %d\n", RS_MAX_POLY_DEGREE-EXTRA_POINTS);
+    printf("Number of errors: rand(%d, %d)\n", 1, EXTRA_POINTS);
+    printf("################ TEST BEGIN ################\n");
     int success = 0;
-    int totalTests = 100;
+
+    struct timespec t0, t1;
+    long long maxElapsed = 0;
+    long long minElapsed = LLONG_MAX;
+    long long averageElapsed = 0;
 
     for(int i = 0; i < totalTests; i++){
-        success += createSimulation(4, RS_MAX_POLY_DEGREE-EXTRA_POINTS, EXTRA_POINTS-1);
+        if (clock_gettime(CLOCK_REALTIME, &t0) != 0) {
+            perror("clock_gettime");
+            return;
+        }
+
+        // Run the simulation.
+        success += createSimulation(RS_MAX_POLY_DEGREE-EXTRA_POINTS, 1, EXTRA_POINTS-1);
+        
+        if (clock_gettime(CLOCK_REALTIME, &t1) != 0) {
+            perror("clock_gettime");
+            return;
+        }
+        long long elapsed = 
+            (long long)(t1.tv_sec * 1000000000LL + t1.tv_nsec) - 
+            (long long)(t0.tv_sec * 1000000000LL + t0.tv_nsec);
+        averageElapsed += elapsed;
+        if(elapsed > maxElapsed){
+            maxElapsed = elapsed;
+        }
+        if(elapsed < minElapsed){
+            minElapsed = elapsed;
+        }
+
+        printLoadingBar(i+1, totalTests);
     }
-    printf("Success: %d/%d\n", success, totalTests);
+    printf("\n############# TEST RESULTS ################\n");
+    printf("Success rate: %d/%d\n", success, totalTests);
+    printf("Average elapsed time: %ld ns\n", averageElapsed/totalTests);
+    printf("Minimum elapsed time: %ld ns\n", minElapsed);
+    printf("Maximum elapsed time: %ld ns\n", maxElapsed);
 }
 
 int testCase(){
-    int y[] = {177, 96, 222, 177, 128, 173, 8};
+    int y[] = {60,     12,     51,     237, 154};
     int numPoints = sizeof(y)/sizeof(int);
 
     int x[numPoints];
@@ -452,74 +622,18 @@ int testCase(){
         x[i] = i;
     }
 
-    Polynomial p = createLagrangeInterp(x, y, numPoints);
+    int errX[] = {0,1};
+    int errY[] = {164,189};
+    int numErrors = sizeof(errX)/sizeof(int);
 
-    int xx[numPoints + EXTRA_POINTS];
-    int yy[numPoints + EXTRA_POINTS];
-
-    for(int i = 0; i < numPoints+EXTRA_POINTS; i++){
-        xx[i] = i;
-
-        Fraction result = evaluatePoly(p, (Fraction){xx[i], 1});
-        result = modFrac(result);
-        
-        if(result.a == -1){
-            printf("Error modding the following message:\n");
-            for(int i = 0; i < numPoints; i++){
-                printf("x = %d  ->  y = %d\n", x[i], y[i]);
-            }
-            exit(-1);
-        }
-        
-        yy[i] = result.a;
-    }
-
-    // Up to this point, the points are generated and "sent".
-    int errory[numPoints + EXTRA_POINTS];
-    memcpy(errory, yy, sizeof(yy));
-    
-    // New error introduced!
-    errory[1] = 65;
-
-    int ry[numPoints + EXTRA_POINTS];
-    memcpy(ry, errory, sizeof(yy));
-
-    // Find the error.
-    int success = verifyMessage(xx, ry, numPoints+EXTRA_POINTS, numPoints);
-    if(success != -1){
-        int sameAsSent = 1;
-        for(int i = 0; i < numPoints+EXTRA_POINTS; i++){
-            sameAsSent &= yy[i] == ry[i];
-        }
-        if(!sameAsSent){
-            printf("points: %d\n", numPoints);
-            printPoly(p);
-            printf("NOT FIXED!\n");
-            for(int i = 0; i < numPoints+EXTRA_POINTS; i++){
-                printf("%d, ", xx[i]);
-            }
-            printf("\nY : ");
-            for(int i = 0; i < numPoints+EXTRA_POINTS; i++){
-                printf("%d, ", yy[i]);
-            }
-            printf("\nEY: ");
-            for(int i = 0; i < numPoints+EXTRA_POINTS; i++){
-                printf("%d, ", errory[i]);
-            }
-            printf("\nRY: ");
-            for(int i = 0; i < numPoints+EXTRA_POINTS; i++){
-                printf("%d, ", ry[i]);
-            }
-            printf("\n\n");
-        }
-        return sameAsSent;
-    }
-    return 0;
+    return runSimulation(x, y, numPoints, errX, errY, numErrors);
 }
 
 /***************************************************************************************************
  * MAIN
  **************************************************************************************************/
 int main(){
-    testCase();
+    srand(time(0));
+    testBench(100000);
+    // testCase();
 }
